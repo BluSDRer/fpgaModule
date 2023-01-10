f849b1bb2666b53d338228b487eb950dd44dffc4
BluSDRer <751788733@qq.com>
2023-01-10 16:02:17 +0800

    pl reset

---
+++ pl_rest/pl_rest/pl_rest.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_pl_rest_test.elf_on_local.tcl
@@ -0,0 +1,18 @@
+connect -url tcp:127.0.0.1:3121
+source C:/Users/jialf/Desktop/work/fpgaModule/pl_rest/pl_rest/pl_rest.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251A08870"} -index 0
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent JTAG-SMT2 210251A08870" && level==0} -index 1
+fpga -file C:/Users/jialf/Desktop/work/fpgaModule/pl_rest/pl_rest/pl_rest.sdk/design_1_wrapper_hw_platform_0/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251A08870"} -index 0
+loadhw -hw C:/Users/jialf/Desktop/work/fpgaModule/pl_rest/pl_rest/pl_rest.sdk/design_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+configparams force-mem-access 1
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251A08870"} -index 0
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251A08870"} -index 0
+dow C:/Users/jialf/Desktop/work/fpgaModule/pl_rest/pl_rest/pl_rest.sdk/pl_rest_test/Debug/pl_rest_test.elf
+configparams force-mem-access 0
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-SMT2 210251A08870"} -index 0
+con
